bd7529ed6a2ad57b62f1883c64e20e94aa8a29b9
christian111 <christian@rightchoice.email>
2023-08-04 06:50:52 +0800

    initial commit

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -4,6 +4,7 @@ import 'package:flutter_restapi/presentation/resources/assets_manager.dart';
 import 'package:flutter_restapi/presentation/resources/color_manager.dart';
 import 'package:flutter_restapi/presentation/resources/strings_manager.dart';
 import 'package:flutter_restapi/presentation/resources/values_manager.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -16,7 +17,9 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   late final List<SliderObject> _list = _getSliderData();
   PageController _pageController = PageController(initialPage: 0);
   int _currentIndex = 0;
-  List<SliderObject> _getSliderData() => [
+
+  List<SliderObject> _getSliderData() =>
+      [
         SliderObject(AppStrings.onBoardingTitle1,
             AppStrings.onBoardingSubTitle1, ImageAssets.onboarding_logo1),
         SliderObject(AppStrings.onBoardingTitle2,
@@ -32,59 +35,140 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     return Scaffold(
       backgroundColor: ColorManager.white,
       appBar: AppBar(
+        backgroundColor: ColorManager.white,
         elevation: AppSize.s1_5,
         systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: ColorManager.white,
-          statusBarBrightness: Brightness.dark,
-          statusBarIconBrightness: Brightness.dark
+            statusBarColor: ColorManager.white,
+            statusBarBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark
         ),
       ),
       body: PageView.builder(
           controller: _pageController,
           itemCount: _list.length,
-          onPageChanged: (index){
-          setState(() {
-            _currentIndex = index;
-          });
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
           },
 
-          itemBuilder: (context,index){
-
+          itemBuilder: (context, index) {
+            return OnBoardingPage(_list[index]);
           }),
+
+      bottomSheet: Container(
+        color: ColorManager.white,
+        height: AppSize.s100,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(onPressed: () {},
+                  child: Text(AppStrings.skip, textAlign: TextAlign.end,)),
+            ),
+            _getBottomSheetWidget()
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _getBottomSheetWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(padding: EdgeInsets.all(AppPadding.p14),
+          child: GestureDetector(
+            child: SizedBox(
+              height: AppSize.s20,
+              width: AppSize.s20,
+              child: SvgPicture.asset(ImageAssets.leftArrowIc),
+            ),
+            onTap: (){
+            _pageController.animateToPage(_getPrevio, duration: duration, curve: curve)
+            },
+          ),
+        ),
+
+        Row(
+          children: [
+            for (int i = 0; i<_list.length; i++)
+              Padding(padding: EdgeInsets.all(AppPadding.p8),
+              child: _getProperCircle(i),
+              )
+          ],
+        ),
+
+
+
+        Padding(padding: EdgeInsets.all(AppPadding.p14),
+          child: GestureDetector(
+            child: SizedBox(
+              height: AppSize.s20,
+              width: AppSize.s20,
+              child: SvgPicture.asset(ImageAssets.rightArrowIc),
+            ),
+            onTap: (){
+
+            },
+          ),
+        )
+      ],
+    );
+  }
+  Widget _getProperCircle(int index){
+    if (index == _currentIndex) {
+      return SvgPicture.asset(ImageAssets.hollowCircleIc);
+    }
+    else{
+      return SvgPicture.asset(ImageAssets.solidCircleIc);
+    }
+  }
+
 }
 
 class OnBoardingPage extends StatelessWidget {
-  SliderObject sliderObject;
-  const OnBoardingPage({required this.sliderObject, super.key});
+  final SliderObject sliderObject;
+
+  const OnBoardingPage(this.sliderObject, { super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
       children: [
         SizedBox(height: AppSize.s40,),
         Padding(
           padding: const EdgeInsets.all(AppPadding.p8),
           child: Text(sliderObject.title,
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.displayLarge,
+            style: Theme
+                .of(context)
+                .textTheme
+                .displayLarge,
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(AppPadding.p8),
           child: Text(sliderObject.subTitle,
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleMedium,
+            style: Theme
+                .of(context)
+                .textTheme
+                .titleMedium,
           ),
         ),
-        const SizedBox(height: AppSize.s60,),
+        SizedBox(height: AppSize.s8),
+
+        SvgPicture.asset(sliderObject.image),
 
       ],
     );
   }
 }
+
+
 
 
 class SliderObject {
--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -7,5 +7,9 @@ class ImageAssets{
   static const String onboarding_logo2 = "$IMAGE_PATH/onboarding_logo2.svg";
   static const String onboarding_logo3 = "$IMAGE_PATH/onboarding_logo3.svg";
   static const String onboarding_logo4 = "$IMAGE_PATH/onboarding_logo4.svg";
+  static const String hollowCircleIc = "$IMAGE_PATH/hollow_cirlce_ic.svg";
+  static const String leftArrowIc = "$IMAGE_PATH/left_arrow_ic.svg";
+  static const String rightArrowIc = "$IMAGE_PATH/right_arrow_ic.svg";
+  static const String solidCircleIc = "$IMAGE_PATH/solid_circle_ic.svg";
 
 }
